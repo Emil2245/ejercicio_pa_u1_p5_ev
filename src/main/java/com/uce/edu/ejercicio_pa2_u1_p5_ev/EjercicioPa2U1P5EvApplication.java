23706982a78bfa1986999281bfe33286883c3678
Emil2245 <emilmontiel@hotmail.com>
2023-12-04 18:16:54 -0500

    .

--- src/main/java/com/uce/edu/ejercicio_pa2_u1_p5_ev/EjercicioPa2U1P5EvApplication.java
+++ src/main/java/com/uce/edu/ejercicio_pa2_u1_p5_ev/EjercicioPa2U1P5EvApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class EjercicioPa2U1P5EvApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EjercicioPa2U1P5EvApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(EjercicioPa2U1P5EvApplication.class, args);
+    }
 
 }
--- src/test/java/com/uce/edu/ejercicio_pa2_u1_p5_ev/EjercicioPa2U1P5EvApplicationTests.java
+++ src/test/java/com/uce/edu/ejercicio_pa2_u1_p5_ev/EjercicioPa2U1P5EvApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class EjercicioPa2U1P5EvApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
