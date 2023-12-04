58abf2431af83ab89f79de3f2979bc888a63c10f
Emil2245 <emilmontiel@hotmail.com>
2023-12-04 18:13:48 -0500

    first commit

---
+++ src/main/java/com/uce/edu/ejercicio_pa2_u1_p5_ev/EjercicioPa2U1P5EvApplication.java
@@ -0,0 +1,13 @@
+package com.uce.edu.ejercicio_pa2_u1_p5_ev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjercicioPa2U1P5EvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjercicioPa2U1P5EvApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uce/edu/ejercicio_pa2_u1_p5_ev/EjercicioPa2U1P5EvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uce.edu.ejercicio_pa2_u1_p5_ev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjercicioPa2U1P5EvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
